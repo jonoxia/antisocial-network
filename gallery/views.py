ef7e87e54a37b8b09291c3dccabe9b78a6db482f
Jono Xia <jonathan.xia@kwhanalytics.com>
2015-03-24 18:24:26 -0700

    Created basic templates for all the main pages

--- gallery/views.py
+++ gallery/views.py
@@ -2,29 +2,49 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.shortcuts import redirect
+from django.template import RequestContext
 from gallery.models import PRIVACY_SETTINGS
 from gallery.models import Human, Gallery, Work
 
 def person_page(request, person):
-    return HttpResponse("Profile page for %s" % person)
+    data = {"person": person}
+    return render_to_response('gallery/personpage.html', data,
+                    context_instance=RequestContext(request))
 
 def gallery_page(request, person, gallery):
-    return HttpResponse("Gallery page for person %s gallery %s" % (person, gallery))
+    data = {"person": person, "gallery": gallery}
+    return render_to_response('gallery/gallerypage.html', data,
+                    context_instance=RequestContext(request))
+
 
 def work_page(request, person, gallery, work):
-    return HttpResponse("Work page for person %s gallery %s work %s" % (person, gallery, work))
+    data = {"person": person, "gallery": gallery, "work": work}
+    return render_to_response('gallery/workpage.html', data,
+                    context_instance=RequestContext(request))
 
 def edit_my_profile(request, person):
-    return HttpResponse("Edit %s profile page" % person)
+    data = {"person": person}
+    return render_to_response('gallery/editprofile.html', data,
+                    context_instance=RequestContext(request))
+
 
 def new_gallery(request, person):
-    return HttpResponse("%s is creating new gallery" % person)
+    data = {"person": person}
+    return render_to_response('gallery/newgallery.html', data,
+                    context_instance=RequestContext(request))
 
 def edit_gallery(request, person, gallery):
-    return HttpResponse("%s is editing %s gallery" % (person, gallery))
+    data = {"person": person, "gallery": gallery}
+    return render_to_response('gallery/editgallery.html', data,
+                    context_instance=RequestContext(request))
 
 def new_work(request, person, gallery):
-    return HttpResponse("%s is creating a new work in gallery %s" % (person, gallery))
+    data = {"person": person, "gallery": gallery}
+    return render_to_response('gallery/newwork.html', data,
+                    context_instance=RequestContext(request))
+
 
 def edit_work(request, person, gallery, work):
-    return HttpResponse("%s is editing work %s in gallery %s" % (person, work, gallery))
+    data = {"person": person, "gallery": gallery, "work": work}
+    return render_to_response('gallery/editwork.html', data,
+                    context_instance=RequestContext(request))
