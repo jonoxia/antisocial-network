0637248fbbba834ccc8d530fdc8413a3b773dc5c
Jono Xia <jono@fastmail.fm>
2026-01-17 21:33:31 -0800

    use STORAGES[default] and STORAGES[staticfiles] in settings.py

--- antisocial/settings.py
+++ antisocial/settings.py
@@ -86,7 +86,14 @@ AWS_DEFAULT_ACL = None
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-
 # Media files storage
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
