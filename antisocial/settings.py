def430249be024c613ac3c0c06550babbf0e442c
Jono Xia <jono@fastmail.fm>
2026-01-17 19:44:03 -0800

    Configuring for using AWS S3 for media file storage

--- antisocial/settings.py
+++ antisocial/settings.py
@@ -66,8 +66,8 @@ ROOT_URLCONF = 'antisocial.urls'
 STATIC_URL = '/static/'
 STATIC_ROOT = Path(BASE_DIR) / 'staticfiles' # was 'static'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = Path(BASE_DIR) / 'collected-media' # was 'media'
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = Path(BASE_DIR) / 'collected-media' # was 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #STATIC_FILES_DIRS =
@@ -75,3 +75,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # optional
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# AWS S3 Settings
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = 'us-west-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+# Media files storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
