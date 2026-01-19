27b0946597d6bcb07ad0ceeff7645a182186c118
Jono Xia <jono@fastmail.fm>
2026-01-19 11:59:00 -0800

    Added a migrate-images command

---
+++ gallery/management/commands/migrate_images.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand
+from gallery.models import Human, Document
+
+class Command(BaseCommand):
+    help = 'Migrates images from Work body to Document model'
+
+    def add_arguments(self, parser):
+        # Optional: add command-line arguments
+        parser.add_argument('--dry-run', action='store_true', help='Preview changes without saving')
+
+    def handle(self, *args, **options):
+        # Your command logic here
+
+        need_bio_img_replacement = Human.objects.filter(portrait__isnull=True)
+        for person in need_bio_img_replacement.all():
+            if person.pictureUrl is not None:
+                self.stdout.write('Person ${name} needs bio img replacement for url ${url}'.format({"name": person.name, "url": person.pictureUrl}))
+
+                # Look up whether there's already a Document?
+                corrected_url = person.pictureUrl.replace("/media", "")
+                doc_match = Document.objects.filter(
+                    docfile == corrected_url,
+                    owner == person
+                    )
+                if doc_match.count() > 0:
+                    person.portrait = doc_match[0]
+                else:
+                    person.portrait = Document(
+                        docfile = corrected_url,
+                        owner = person
+                    )
+                person.save()
+        
+        self.stdout.write(self.style.SUCCESS('Command executed successfully'))
