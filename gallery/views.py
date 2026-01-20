8c6fd0f07fdb73344a046e67f8b5525d9db64d0b
Jono Xia <jono@fastmail.fm>
2026-01-20 01:05:51 -0800

    Correct missing import

--- gallery/views.py
+++ gallery/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import redirect
+from django.conf import settings
 
 import markdown
 import datetime
