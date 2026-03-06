7d9ec0b19cdbd14cbc7eacacccefbd662da6dd9a
Jonathan Xia <jono@fastmail.fm>
2026-03-05 18:44:36 -0800

    Added models to admin interface

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,22 @@
+# Register your models here.
+from .models import Subscriber
+from .models import Work
+from .models import Gallery
+from .models import Document
+from .models import Human
+from .models import Tag
+from .models import SubscriberBeenNotified
+
+
 from django.contrib import admin
 
-# Register your models here.
+
+admin.site.register(Subscriber)
+admin.site.register(Work)
+admin.site.register(Gallery)
+admin.site.register(Document)
+admin.site.register(Human)
+admin.site.register(Tag)
+admin.site.register(SubscriberBeenNotified)
+
+
