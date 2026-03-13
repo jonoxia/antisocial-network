9f451616ceac840ecd27fdd754b17fa5ff3412c5
Jonathan Xia <jono@fastmail.fm>
2026-03-12 17:04:38 -0700

    Made admin page easier to use

--- gallery/admin.py
+++ gallery/admin.py
@@ -11,12 +11,20 @@ from .models import SubscriberBeenNotified
 from django.contrib import admin
 
 
-admin.site.register(Subscriber)
 admin.site.register(Work)
 admin.site.register(Gallery)
 admin.site.register(Document)
-admin.site.register(Human)
-admin.site.register(Tag)
 admin.site.register(SubscriberBeenNotified)
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("tagText",)
 
+
+@admin.register(Human)
+class HumanAdmin(admin.ModelAdmin):
+    list_display = ("publicName",)
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ("subscriber_name", "email", "phone", "url", "contact_method")
